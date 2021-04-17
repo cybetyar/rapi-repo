8d6c1d3ea20534da111f06e386b0ee863f501d24
BL4CKW3LL <49598798+cybetyar@users.noreply.github.com>
2021-04-17 17:53:53 +0200

    Generate QR code from given hash value

--- static/qr_generator.js
+++ static/qr_generator.js
@@ -1,6 +1,4 @@
-var max_id = parseInt(document.getElementById('max_id').innerHTML);
-var current_id = max_id + 1;
-
-$(function() {
-    $(".qr_code").attr("src", "https://chart.googleapis.com/chart?cht=qr&chl=" + current_id + "&chs=160x160&chld=L|0");
-});
+    var new_qrcode = String(document.getElementById('new_qrcode').innerHTML);
+    $(function() {
+        $(".qr_code").attr("src", "https://chart.googleapis.com/chart?cht=qr&chl=" + new_qrcode + "&chs=160x160&chld=L|0");
+    });
