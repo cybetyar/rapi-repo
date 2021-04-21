0c7c4602517a1a05337c8f25eebb74709115fd27
BL4CKW3LL <49598798+cybetyar@users.noreply.github.com>
2021-04-21 17:19:48 +0200

    Delete qr_generator.js
    Easier to write those 3 lines inline

--- static/qr_generator.js
+++
@@ -1,4 +0,0 @@
-    var new_qrcode = String(document.getElementById('new_qrcode').innerHTML);
-    $(function() {
-        $(".qr_code").attr("src", "https://chart.googleapis.com/chart?cht=qr&chl=" + new_qrcode + "&chs=160x160&chld=L|0");
-    });
